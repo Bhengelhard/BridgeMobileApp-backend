8fa1ef06501ab0ac3a95db2a8a03a9cd13adf5a7
SAGAR SINHA <sagar0327@gmail.com>
2016-07-11 10:26:47 -0400

    background job added as function 2

--- cloud/main.js
+++ cloud/main.js
@@ -3,7 +3,8 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('helrlo');
 });
 Parse.Cloud.define('addBridgePairing', function(req, res) {
-var bridgePairing = new BridgePairings();
+var BridgePairingsClass = Parse.Object.extend("BridgePairings");
+var bridgePairing = new BridgePairingsClass();
 bridgePairing.set("user1","001")
 bridgePairing.set("user2","002")
 bridgePairing.set("bridge_type","Business")
