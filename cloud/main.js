ad8659c8fd0915a306a568ef51c5619ac91f296c
SAGAR SINHA <sagar0327@gmail.com>
2016-07-11 10:14:06 -0400

    background job removed

--- cloud/main.js
+++ cloud/main.js
@@ -2,18 +2,3 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('helrlo');
 });
-
-Parse.Cloud.job(“updateTable”, function(request, status) {
-                var bridgePairing = new BridgePairings();
-                bridgePairing.set("user1","001")
-                bridgePairing.set("user2","002")
-                bridgePairing.set("bridge_type","Business")
-                bridgePairing.save(null, {
-                                   success: function(bridgePairing){
-                                    status.success("Saved")
-                                   },
-                                   error: function(bridgePairing, error){
-                                    status.error("Not saved")
-                                   }
-                                   });
-});
