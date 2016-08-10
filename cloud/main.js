2809c1ac462de650d554e1f6c04e9babc0886ec0
SAGAR SINHA <sagar0327@gmail.com>
2016-08-10 09:03:04 -0400

    recreate pairings on interestedIn update and update on status update

--- cloud/main.js
+++ cloud/main.js
@@ -33,7 +33,7 @@ Parse.Cloud.define('changeBridgePairingsOnStatusUpdate', function(req, res) {
                              for (var i = 0, len = results.length; i < len; i++) {
                              var result = results[i];
                              var userObjectIds = result["user_objectIds"];
-                             console.log("userObjectIds[0]="+userObjectIds[0] + " & userObjectIds[1]= "+userObjectIds[1]);
+                             console.log("result = "+ result + "userObjectIds[0]="+userObjectIds[0] + " & userObjectIds[1]= "+userObjectIds[1]);
                              if( userObjectIds.length > 0 ){
                              if (userObjectIds[0] == req.user.id) {
                                 result["user1_bridge_status"] = req.params.status;
