622185b4caf165a95754d483fef74eed3afd4103
SAGAR SINHA <sagar0327@gmail.com>
2016-08-22 14:36:46 -0400

    production=false

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ var api = new ParseServer({
         pfx: 'Parse_Push_Development_Certificate.p12',
         passphrase: '', // optional password to your p12/PFX
         bundleId: 'com.Bridge.Beta',
-        production: true
+        production: false
     }
  }
                         
