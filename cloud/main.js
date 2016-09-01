236228180087454a71b709d2288494f671d01d93
Blake Engelhard <bhengelhard@gmail.com>
2016-09-01 19:57:13 -0400

    addIntroducedUsersToEachothersFriendLists cloud code defined to make user's friends when they are introduced

--- cloud/main.js
+++ cloud/main.js
@@ -219,11 +219,18 @@ Parse.Cloud.define('addIntroducedUsersToEachothersFriendLists', function(req, re
                userObjectIdToAdd = req.params.userObjectId2;
                console.log("adding to friend_list user1 " + userObjectIdToAdd);
                }
-               result.addUnique("friendlist",userObjectIdToAdd);
+               result.addUnique("friendlist", userObjectIdToAdd);
                console.log("addIntroducedUsersToEachothersFriendLists");
                //result.set("friend_list", ["test"]);
-               result.save(null, {
-                           success: function(){
+               post.save(null, { useMasterKey: true }).then(function() {
+                                                            response.success();
+                                                            }, function(error) {
+                                                            response.error(error);
+                                                            });
+               
+               
+               result.save(null, { userMasterKey: true }).then
+                           success: function(result){
                            console.log("Saved after adding Introduced Users To Eachothers Friend Lists")
                            incrementWhenDone.count += 1;
                            //once incrementWhenDone get to the length of the results, it is clear the job has completed - this is necessary due to asynchronous execution
@@ -234,7 +241,7 @@ Parse.Cloud.define('addIntroducedUsersToEachothersFriendLists', function(req, re
                            }
                            
                            },
-                           error: function(error){
+                           error: function(result, error){
                            console.log(" Not Saved after adding objectId's to friend_list in User Table")
                            incrementWhenDone.count += 1;
                            if (incrementWhenDone.count == results.length) {
