a33ed9b6f87bf17385c5983a9dd754ec80d813ff
Blake Engelhard <bhengelhard@gmail.com>
2016-09-01 12:32:53 -0400

    addIntroducedUsersToEachothersFriendLists cloud code defined to make user's friends when they are introduced

--- cloud/main.js
+++ cloud/main.js
@@ -189,7 +189,7 @@ Parse.Cloud.define('addIntroducedUsersToEachothersFriendLists', function(req, re
     //query passing the classname -> which is the name of the table being queried
     var query = new Parse.Query(UserClass);
     //queries the table for the id's that include the user's introduced
-    var introducedUserIds = [req.user_objectId1, req.user_objectId2]
+    var introducedUserIds = [req.params.userObjectId1, req.params.userObjectId2]
     query.equalTo("objectId", introducedUserIds);
     console.log("introducedUserIds = " + introducedUserIds)
     query.limit = 2
